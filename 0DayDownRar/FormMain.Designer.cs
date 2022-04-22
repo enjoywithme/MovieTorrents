6ef7f936f486edebc0a3b5cd0144698bd2823c8b
Duan Xin <xxinduan@hotmail.com>
2022-04-23 02:16:00 +0800

    添加处理 0daydown.com 下载软件解压/压缩的工具

---
+++ 0DayDownRar/FormMain.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace _0DayDownRar
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSelectSourceFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbWinrarPath = new System.Windows.Forms.TextBox();
+            this.btSelectWinrarPath = new System.Windows.Forms.Button();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btClear = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源路径";
+            // 
+            // btSelectSourceFolder
+            // 
+            this.btSelectSourceFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSelectSourceFolder.Location = new System.Drawing.Point(856, 22);
+            this.btSelectSourceFolder.Name = "btSelectSourceFolder";
+            this.btSelectSourceFolder.Size = new System.Drawing.Size(34, 23);
+            this.btSelectSourceFolder.TabIndex = 0;
+            this.btSelectSourceFolder.Text = "...";
+            this.btSelectSourceFolder.UseVisualStyleBackColor = true;
+            this.btSelectSourceFolder.Click += new System.EventHandler(this.BtnSelectSourceFolderClick);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Winrar路径";
+            // 
+            // tbWinrarPath
+            // 
+            this.tbWinrarPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbWinrarPath.Location = new System.Drawing.Point(12, 462);
+            this.tbWinrarPath.Name = "tbWinrarPath";
+            this.tbWinrarPath.Size = new System.Drawing.Size(808, 21);
+            this.tbWinrarPath.TabIndex = 4;
+            this.tbWinrarPath.Tag = "";
+            this.tbWinrarPath.Text = "C:\\Program Files\\WinRAR\\";
+            // 
+            // btSelectWinrarPath
+            // 
+            this.btSelectWinrarPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSelectWinrarPath.Location = new System.Drawing.Point(856, 456);
+            this.btSelectWinrarPath.Name = "btSelectWinrarPath";
+            this.btSelectWinrarPath.Size = new System.Drawing.Size(34, 23);
+            this.btSelectWinrarPath.TabIndex = 6;
+            this.btSelectWinrarPath.Text = "...";
+            this.btSelectWinrarPath.UseVisualStyleBackColor = true;
+            this.btSelectWinrarPath.Click += new System.EventHandler(this.BtSelectWinrarPathClick);
+            // 
+            // btStart
+            // 
+            this.btStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btStart.Location = new System.Drawing.Point(839, 164);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(69, 124);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "开始";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.TbnStartClick);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 486);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "处理进度";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorkerDoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorkerProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorkerRunWorkerCompleted);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(808, 420);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "路径";
+            this.columnHeader1.Width = 360;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "状态";
+            // 
+            // btClear
+            // 
+            this.btClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btClear.Location = new System.Drawing.Point(856, 51);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(34, 26);
+            this.btClear.TabIndex = 1;
+            this.btClear.Text = "X";
+            this.btClear.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 507);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(808, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "处理消息";
+            this.columnHeader3.Width = 300;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 543);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.btSelectWinrarPath);
+            this.Controls.Add(this.tbWinrarPath);
+            this.Controls.Add(this.btSelectSourceFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "0DayDown RAR";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMainFormClosing);
+            this.Load += new System.EventHandler(this.FormMainLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSelectSourceFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbWinrarPath;
+        private System.Windows.Forms.Button btSelectWinrarPath;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ 0DayDownRar/WinRar.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _0DayDownRar
+{
+    public class WinRar
+    {
+        private readonly string _winRarPath;
+        private readonly string _workPath;
+        private const string ZeroDownPwd = "0daydown";
+
+        public WinRar(string winRarPath, string workPath)
+        {
+            _winRarPath = winRarPath;
+            _workPath = workPath;
+        }
+
+        private static readonly Dictionary<int, string> WinRarError = new Dictionary<int, string>()
+        {
+            { 1, " 警告。发生非致命错误" },
+            { 2, " 发生致命错误。" },
+            { 3, "无效校验和。数据损坏。 " },
+            { 4, " 尝试修改一个 锁定的压缩文件。 " },
+            { 5, " 写错误。 " },
+            { 6, "文件打开错误。 " },
+            { 7, "错误命令行选项。 " },
+            { 8, "内存不足。 " },
+            { 9, "文件创建错误。 " },
+            { 10, " 没有找到与指定的掩码和选项匹配的文件。 " },
+            { 11, " 密码错误。 " },
+            { 255, " 用户中断。 " }
+        };
+
+        public (int, string) TestFile(string fileName)
+        {
+            var cmd = $" t -r -pC3ED2E2B-D14F-4FF0-8EC3-A6142A5AE2F7 -inul -ibck \"{fileName}\"";
+            var (exitCode,message,_) = RunWinExe(Path.Combine(_winRarPath,"winrar.exe"), cmd);
+            return (exitCode, message);
+        }
+
+        public string[] ListContent(string fileName)
+        {
+            var cmd = $" lb -p{ZeroDownPwd} -v \"{fileName}\"";
+            var (exitCode, message, output) = RunWinExe(Path.Combine(_winRarPath, "unrar.exe"), cmd);
+            if (exitCode!=0) throw new Exception(message);
+            Debug.WriteLine(output);
+            //return Regex.Split(output, "\r\n|\r|\n");
+            return output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public (int,string) ExtractFile(string fileName, string extractDir)
+        {
+            var cmd = $"x -IBCK -o+ -y -inul -p{ZeroDownPwd} \"{fileName}\"  \"{extractDir}\"";//背景解压，覆盖文件不确认,禁止显示错误
+            var (exitCode, message, _) = RunWinExe(Path.Combine(_winRarPath, "winrar.exe"), cmd);
+            return (exitCode, message);
+        }
+
+        public (int, string) CompressFolder(string fileName, string extractDir)
+        {
+            var cmd = $"a -r -rr3p -v333m -vn -ep1 -df -t \"{fileName}\"  \"{extractDir}\"";
+            var (exitCode, message, _) = RunWinExe(Path.Combine(_winRarPath, "rar.exe"), cmd);
+            return (exitCode, message);
+        }
+
+        public void DeleteRarFiles(string fileName)
+        {
+            var name = Path.GetFileName(fileName);
+
+            var match = Regex.Match(name, @"\.part\d*.rar",RegexOptions.IgnoreCase);
+            if(!match.Success)
+            {
+                File.Delete(fileName);
+                return;
+            }
+            var dir = Path.GetDirectoryName(fileName);
+            if (string.IsNullOrEmpty(dir))
+                throw new Exception("路径非法。");
+            var s = name.Replace(match.Groups[0].Value, @"\.part\d*.rar");
+            var files = Directory.GetFiles(dir);
+            foreach (var file in files)
+            {
+                if(Regex.Match(Path.GetFileName(file),s).Success)
+                    File.Delete(file);
+            }
+
+
+        }
+
+        private (int,string,string) RunWinExe(string exe,string cmd)
+        {
+            Debug.WriteLine($"{exe} {cmd}");
+            var procStartInfo = new ProcessStartInfo(exe, cmd)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = _workPath
+            };
+            var proc = new Process { StartInfo = procStartInfo };
+            proc.Start();
+            var output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            //var result = proc.StandardError.ReadToEnd();
+            var errCode = WinRarError.ContainsKey(proc.ExitCode) ? WinRarError[proc.ExitCode] : "未知错误";
+            return proc.ExitCode != 0 ? (proc.ExitCode,errCode,output) : (proc.ExitCode, "",output);
+        }
+
+        public static string RarBaseName(string fileName)
+        {
+            var name = Path.GetFileName(fileName);
+
+            var match = Regex.Match(name, @"\.part\d*.rar",RegexOptions.IgnoreCase);
+            return !match.Success ? Path.GetFileNameWithoutExtension(fileName) : name.Replace(match.Groups[0].Value, "");
+        }
+    }
+}
