61d40d5829317e8fdaa7907dcdd757e03f7956cf
Duan Xin <xxinduan@hotmail.com>
2022-11-18 12:30:22 +0800

    - Add extract html embedded base64 image to local file

---
+++ CleanHtml/FormOption.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CleanHtml
+{
+    partial class FormOption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btOk = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 137);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Images";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(37, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(149, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Clean iamge lazy load";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(37, 77);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(161, 16);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.Text = "Extract embedded images";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btOk
+            // 
+            this.btOk.Location = new System.Drawing.Point(90, 209);
+            this.btOk.Name = "btOk";
+            this.btOk.Size = new System.Drawing.Size(75, 23);
+            this.btOk.TabIndex = 1;
+            this.btOk.Text = "&Ok";
+            this.btOk.UseVisualStyleBackColor = true;
+            this.btOk.Click += new System.EventHandler(this.btOk_Click);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(189, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "&Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormOption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 276);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btOk);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormOption";
+            this.Text = "Option";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btOk;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ CleanHtml/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CleanHtml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+            var htmlFile = args[0];
+            Debug.WriteLine(htmlFile);
+            if (!File.Exists(htmlFile)) return;
+
+            var formOption = new FormOption();
+            var ret = formOption.ShowDialog();
+            if (ret == DialogResult.Cancel) return;
+
+            switch (formOption.Action)
+            {
+                case FormOption.ActionType.CleanImageLazyLoad:
+                    CleanImageLazyLoad(htmlFile);
+                    break;
+
+                case FormOption.ActionType.ExtractEmbeddedImage:
+                    ExtractBase64Image(htmlFile);
+                    break;
+            }
+
+        }
+
+        private static void CleanImageLazyLoad(string htmlFile)
+        {
+            var content = File.ReadAllText(htmlFile);
+
+            content = Regex.Replace(content, "data-src=[^\\s]*?\\s", "");
+            content = Regex.Replace(content, "data-srcset=\".*?\"", "");
+
+            File.WriteAllText(htmlFile, content);
+        }
+
+        private static void ExtractBase64Image(string htmlFile)
+        {
+            var content = File.ReadAllText(htmlFile);
+
+            content = Regex.Replace(content, "data-src=[^\\s]*?\\s", "");
+            content = Regex.Replace(content, "data-srcset=\".*?\"", "");
+
+            var sb = new StringBuilder(content);
+
+            var filePath = Path.GetDirectoryName(htmlFile);
+            if(!Directory.Exists(filePath)) return;
+
+            var match = Regex.Match(content, "\\<img[\\s]*src=\"data:image\\/([\\S]*);base64,([\\S]*)\"");
+            var ret = false;
+            while (match.Success)
+            {
+                if (match.Groups.Count >= 3)
+                {
+                    var ext = match.Groups[1].Value;
+                    var base64 = match.Groups[2].Value;
+
+                    var guid = Guid.NewGuid().ToString("D");
+                    var img = $"{guid}.{ext}";
+
+                    var data = Convert.FromBase64String(base64);
+                    File.WriteAllBytes(Path.Combine(filePath,img),data);
+
+                    sb.Replace($"data:image/{ext};base64,{base64}", img);
+
+                    ret = true;
+
+                }
+                
+
+                match = match.NextMatch();
+            }
+
+            if (!ret) return;
+
+            var newFile = Path.Combine(filePath, Path.GetFileNameWithoutExtension(htmlFile) + "_1"+ Path.GetExtension(htmlFile));
+
+            Debug.WriteLine(newFile);
+
+            File.WriteAllText(newFile,sb.ToString());
+
+        }
+    }
+}
