9e48b052fed60f1084363e1b9583906032952963
Duan Xin <xxinduan@hotmail.com>
2023-01-27 00:33:23 +0800

    - 根据learnopencv保存的页面调整页面清洗

--- CleanHtml/Program.cs
+++ CleanHtml/Program.cs
@@ -11,8 +11,17 @@ namespace CleanHtml
     {
         static void Main(string[] args)
         {
+#if DEBUG
+
+            var htmlFile =
+                "X:\\temp\\Building An Automated Image Annotation Tool_ PyOpenAnnotate (2023_1_26 20_39_47).html";
+#else
             if (args.Length == 0) return;
+
             var htmlFile = args[0];
+#endif
+
+
             Debug.WriteLine(htmlFile);
             if (!File.Exists(htmlFile)) return;
 
@@ -37,48 +46,78 @@ namespace CleanHtml
         {
             var content = File.ReadAllText(htmlFile);
 
-            content = Regex.Replace(content, "data-src=[^\\s]*?\\s", "");
-            content = Regex.Replace(content, "data-srcset=\".*?\"", "");
+            content = ClearImageLazy(content);
 
             File.WriteAllText(htmlFile, content);
+        }
+
+        private static string ClearImageLazy(string content)
+        {
+            content = Regex.Replace(content, "data-src=[^\\s]*?\\s", "");
+            content = Regex.Replace(content, "data-lazy-src=[^\\s]*?\\s", "");
+
+            content = Regex.Replace(content, "data-srcset=\".*?\"", "");
+            content = Regex.Replace(content, "data-lazy-srcset=\".*?\"", "");
+
+            content = Regex.Replace(content, "data-lazy-sizes=\".*?\"", "");
+
+            content = Regex.Replace(content, "data-ll-status=[^\\s]*?\\s", "");
+
+            return content;
         }
 
         private static void ExtractBase64Image(string htmlFile)
         {
             var content = File.ReadAllText(htmlFile);
 
-            content = Regex.Replace(content, "data-src=[^\\s]*?\\s", "");
-            content = Regex.Replace(content, "data-srcset=\".*?\"", "");
+            content = ClearImageLazy(content);
+
 
             var sb = new StringBuilder(content);
 
             var filePath = Path.GetDirectoryName(htmlFile);
             if(!Directory.Exists(filePath)) return;
 
-            var match = Regex.Match(content, "\\<img[\\s]*src=\"data:image\\/([\\S]*);base64,([\\S]*)\"");
+            //\<img[^\>\<]*src="?data:image\/([a-z]*);base64,([\S]+)\s+
+            var match = Regex.Match(content, @"\<img[^\>\<]*src=""?data:image\/([a-z]*);base64,([^""\s]+)");
             var ret = false;
+            var i = 0;
             while (match.Success)
             {
+                i++;
                 if (match.Groups.Count >= 3)
                 {
-                    var ext = match.Groups[1].Value;
-                    var base64 = match.Groups[2].Value;
+                    try
+                    {
+                        var ext = match.Groups[1].Value;
+                        var base64 = match.Groups[2].Value;
 
-                    var guid = Guid.NewGuid().ToString("D");
-                    var img = $"{guid}.{ext}";
+                        var guid = Guid.NewGuid().ToString("D");
+                        var img = $"{guid}.{ext}";
 
-                    var data = Convert.FromBase64String(base64);
-                    File.WriteAllBytes(Path.Combine(filePath,img),data);
+                        var data = Convert.FromBase64String(base64);
+                        File.WriteAllBytes(Path.Combine(filePath, img), data);
 
-                    sb.Replace($"data:image/{ext};base64,{base64}", img);
+                        sb.Replace($"data:image/{ext};base64,{base64}", img);
 
-                    ret = true;
+                        ret = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+#if DEBUG
+                        File.WriteAllText(Path.Combine(filePath, $"{i}.txt"), match.Groups[2].Value);
+#endif
+                    }
+
 
                 }
                 
 
                 match = match.NextMatch();
             }
+
+            Debug.WriteLine(i.ToString());
 
             if (!ret) return;
 
