08a8dfab2c5556415a854e61bf1b698eebc2e397
Duan Xin <xxinduan@outlook.com>
2023-11-18 23:58:13 +0800

    mypages - delete full text index

--- mySharedLib/FuncResult.cs
+++ mySharedLib/FuncResult.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace mySharedLib
 {
     public class FuncResult
     {
-        public bool Ret { get; private set; } = true;
+        public bool Success { get; private set; } = true;
         private readonly StringBuilder _stringBuilder = new();
 
         public int ErrorCount { get; private set; }
         public string Message => _stringBuilder.ToString().TrimEnd();
 
-        public static implicit operator bool(FuncResult d) => d.Ret;
+        public static implicit operator bool(FuncResult d) => d.Success;
         public void Set(bool ret, string msg)
         {
-            Ret = Ret && ret;
+            Success = Success && ret;
             if (!ret) ErrorCount++;
             _stringBuilder.AppendLine(msg);
         }
